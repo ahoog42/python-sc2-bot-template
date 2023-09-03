937abab61808570b4f3895ae068d3accc17a8bb2
Andrew Hoog <ahoog42@gmail.com>
2023-09-02 21:01:31 -0500

    improve build logic, build 6 barracks, attack enemy in groups on 10 marines

--- bot/bot.py
+++ bot/bot.py
@@ -36,17 +36,72 @@ class CompetitiveBot(BotAI):
         Populate this function with whatever your bot should do!
         """
 
-        await self._client.debug_create_unit([[UnitTypeId.SUPPLYDEPOT, 1, self.start_location.towards(self.game_info.map_center, 5), MY_PLAYER_ID]])
+        # build scv first time around only
+        if iteration == 0:
+            await self.build_workers()
+
+        if self.workers.idle:
+            await self.distribute_workers();
+
+        await self.build_supply();
+        await self.build_barracks();
+        await self.train_marines();
+        await self.attack_enemy_location();
+
+        #await self.build_barracks()
 
         if (self.units or self.structures) and (self.enemy_units or self.enemy_structures):
             self.enemy_location = (self.enemy_units + self.enemy_structures).center
             self.fight_started = True
 
-        await self.manage_own_units()
+    async def build_workers(self):
+        for cc in self.townhalls(UnitTypeId.COMMANDCENTER).ready.idle:
+            if self.can_afford(UnitTypeId.SCV):
+                cc.train(UnitTypeId.SCV)
 
-    async def manage_own_units(self):
-        for unit in self.units(UnitTypeId.MARINE):
-            unit.attack(self.enemy_location)
+    async def expand(self):
+        if self.townhalls(UnitTypeId.COMMANDCENTER).amount < 3 and self.can_afford(UnitTypeId.COMMANDCENTER):
+            await self.expand_now()
+
+    async def build_supply(self):
+        ccs = self.townhalls(UnitTypeId.COMMANDCENTER).ready
+        if ccs.exists:
+            cc = ccs.first
+            if self.supply_left < 4 and not self.already_pending(UnitTypeId.SUPPLYDEPOT):
+                if self.can_afford(UnitTypeId.SUPPLYDEPOT):
+                    await self.build(UnitTypeId.SUPPLYDEPOT, near=cc.position.towards(self.game_info.map_center, 5))
+                    # await self.distribute_workers();
+
+    async def train_marines(self):
+        #first_barracks = self.townhalls(UnitTypeId.BARRACKS).ready
+        #if first_barracks.exists and self.can_afford(UnitTypeId.MARINE):
+        self.train(UnitTypeId.MARINE, 1);
+
+    async def build_barracks(self):
+        if not self.already_pending(UnitTypeId.BARRACKS):
+            #worker = self.select_build_worker()
+            ccs = self.townhalls(UnitTypeId.COMMANDCENTER).ready
+            if ccs.exists:
+                cc = ccs.first
+                # Can return None
+                #if worker and (await self.can_place(UnitTypeId.BARRACKS, [barracks_placement_position])[0]:
+                if self.can_afford(UnitTypeId.BARRACKS) and self.units(UnitTypeId.BARRACKS).amount < 6:
+                    await self.build(UnitTypeId.BARRACKS, near=cc);
+
+    def find_target(self, state):
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures) > 0:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack_enemy_location(self):
+        if self.units(UnitTypeId.MARINE).amount > 9:
+            #if len(self.known_enemy_units) > 0:
+            for s in self.units(UnitTypeId.MARINE).idle:
+                #await self.do(s.attack(self.find_target(self.state)))
+                self.do(s.attack(self.enemy_start_locations[0]))
             # TODO: implement your fight logic here
             # if unit.weapon_cooldown != 0:
             #     unit.move(u.position.towards(self.start_location))
